ad847926f0507d0ceda12ddfd3f63a2c7e2ea002
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-11-22 10:59:20 +0100

    Add files via upload

---
+++ Configuration&JsonConnectionStrings/BusinessLayer/LogDataManager.cs
@@ -0,0 +1,49 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer {
+   public class LogDataManager {
+        private bool debug;
+        private ADOLogDAO dao;
+
+        public LogDataManager(ADOLogDAO dao, bool debug = false) {
+            this.dao = dao;
+            this.debug = debug;
+        }
+        public string LogToDB(string logInfo) {
+            if (!string.IsNullOrWhiteSpace(logInfo)) {
+                try {
+                    dao.WriteToDB(logInfo);
+                    if (debug)
+                        Console.WriteLine($"writing {logInfo}");
+                    return "success";
+                } catch (DAOException ex) {
+                    if (debug)
+                        Console.WriteLine(ex);
+                    return "fail";
+                }
+            }
+            throw new LogException("loginfo empty");
+        }
+        public void ShowLogdata(int rows = 5) {
+            try {
+                foreach (var x in dao.ReadFromDB(rows)) {
+                    Console.WriteLine($"{x.Item1},{x.Item2}");
+                }
+            } catch (DAOException ex) {
+                if (debug)
+                    Console.WriteLine("ex", ex);
+                Console.WriteLine("no data");
+            } finally {
+                Console.WriteLine("--------");
+            }
+        }
+    }
+
+}
---
+++ Configuration&JsonConnectionStrings/BusinessLayer/LogException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BusinessLayer {
+    [Serializable]
+    public class LogException : Exception {
+        public LogException() {
+        }
+
+        public LogException(string message) : base(message) {
+        }
+
+        public LogException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected LogException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
