4e3a36ecf8fc034b87a877d83866b9857072743f
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-12-01 11:12:12 +0100

    Add files via upload

---
+++ StripProjectEFWithManager/Strip/Data/StripsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StripProjectEF.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StripProjectEF {
+   public class StripsContext : DbContext{
+        public DbSet<Strip> Strips { get; set; }
+        public DbSet<Auteur> Auteurs { get; set; }
+        public DbSet<Reeks> Reeksen { get; set; }
+        public DbSet<Uitgeverij> Uitgeverijen { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=StripsDB;Integrated Security=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<AuteurStrip>().HasKey(x => new { x.StripID, x.AuteurID });
+        }
+    }
+}
