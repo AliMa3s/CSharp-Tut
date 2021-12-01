8b13a3587b1e476972a72bed392f4cabab5179c1
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-12-01 11:12:37 +0100

    Add files via upload

---
+++ StripProjectEFWithManager/Strip/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StripProjectEF {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\alima\source\repos\StripProject\Strip\Data\StripsList.txt";
+            StripManager SM = new StripManager();
+            //SM.InitialiseerDatabank(path);
+            //SM.ToonReeksen();
+            //SM.ToonReeksenMetStrip();
+            SM.ToonStrips();
+            //SM.ToonStripsInclude();
+            //SM.ToonStripsFilter();
+            //SM.ToonStripsIncludeAsNoTracking();
+            //SM.UpdateStrip();
+            //SM.VerwijderStrip();
+        }
+    }
+    
+}
